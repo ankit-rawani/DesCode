7719e85a90eeb22398b17a855bbef665e9e60c8a
Sneha <sneharmkrshnn@gmail.com>
2022-03-05 23:37:58 +0530

    [add] Mobile Menu

---
+++ src/components/mobileMenu/index.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import classes from './style.module.css'
+import { useNavigate } from 'react-router-dom'
+
+import logo from '../../images/Light.png'
+import cross from '../../images/icons/Cross.png'
+
+const MobileMenu = ({ setMobile }) => {
+    const navigate = useNavigate()
+    return (
+        <div>
+            <div className={classes.cont}>
+                <div className={classes.container}>
+                    <img
+                        src={logo}
+                        alt="Main Logo"
+                        className={classes.img}
+                        onClick={() => {
+                            navigate('/')
+                        }}
+                    />
+                    <img
+                        src={cross}
+                        alt="Cross"
+                        className={classes.img}
+                        onClick={() => {
+                            setMobile(false)
+                        }}
+                    />
+                </div>
+
+                <div className={classes.navWrapper}>
+                    <div
+                        className={classes.navLink}
+                        onClick={() => {
+                            setMobile(false)
+                            navigate('/explore')
+                        }}
+                    >
+                        Explore
+                    </div>
+                    <div
+                        className={classes.navLink}
+                        onClick={() => {
+                            navigate('/my-items')
+                            setMobile(false)
+                        }}
+                    >
+                        My Items
+                    </div>
+                    <div
+                        className={classes.navLink}
+                        onClick={() => {
+                            navigate('/following')
+                            setMobile(false)
+                        }}
+                    >
+                        Following
+                    </div>
+                </div>
+
+                <div className={classes.navFooter}>
+                    <div className={classes.footerWrapper}>
+                        <div>
+                            <button
+                                className={classes.button}
+                                onClick={() => {
+                                    navigate('/create')
+                                    setMobile(false)
+                                }}
+                            >
+                                Create
+                            </button>
+                        </div>
+                        <div>
+                            <button
+                                className={classes.buttonOutline}
+                                onClick={() => {
+                                    navigate('/connect')
+                                    setMobile(false)
+                                }}
+                            >
+                                Connect
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MobileMenu
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,85 +1,102 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './style.module.css'
 import { useNavigate, useLocation } from 'react-router-dom'
 
 import logo from '../../images/Light2.png'
 import menu from '../../images/icons/Menu.png'
+import MobileMenu from '../mobileMenu/index.js'
 
 export default function Nav() {
     const navigate = useNavigate()
+    const [mobileMenu, setMobile] = useState(false)
     const { pathname } = useLocation()
 
     return (
-        <div className={styles.container}>
-            <div>
-                <img
-                    src={logo}
-                    alt="Main Logo"
-                    className={styles.img}
-                    onClick={() => {
-                        navigate('/')
-                    }}
+        <div>
+            {mobileMenu ? (
+                <MobileMenu
+                    setMobile={setMobile}
+                    className={styles.mobileMenu}
                 />
-            </div>
-            <div className={styles.searchContainer}>
-                <input
-                    className={styles.searchBar}
-                    type="text"
-                    placeholder="Search item here"
-                />
-            </div>
-            <div
-                className={`${styles.navLink} ${
-                    pathname === '/explore' ? styles.navLinkActive : null
-                }`}
-                onClick={() => {
-                    navigate('/explore')
-                }}
-            >
-                Explore
-            </div>
-            <div
-                className={`${styles.navLink} ${
-                    pathname === '/my-items' ? styles.navLinkActive : null
-                }`}
-                onClick={() => {
-                    navigate('/my-items')
-                }}
-            >
-                My Items
-            </div>
-            <div
-                className={`${styles.navLink} ${
-                    pathname === '/following' ? styles.navLinkActive : null
-                }`}
-                onClick={() => {
-                    navigate('/following')
-                }}
-            >
-                Following
-            </div>
-            <div>
-                <button
-                    className={styles.button}
+            ) : null}
+            <div className={styles.container}>
+                <div>
+                    <img
+                        src={logo}
+                        alt="Main Logo"
+                        className={styles.img}
+                        onClick={() => {
+                            navigate('/')
+                        }}
+                    />
+                </div>
+                <div className={styles.searchContainer}>
+                    <input
+                        className={styles.searchBar}
+                        type="text"
+                        placeholder="Search item here"
+                    />
+                </div>
+                <div
+                    className={`${styles.navLink} ${
+                        pathname === '/explore' ? styles.navLinkActive : null
+                    }`}
                     onClick={() => {
-                        navigate('/create')
+                        navigate('/explore')
                     }}
                 >
-                    Create
-                </button>
-            </div>
-            <div>
-                <button
-                    className={styles.buttonOutline}
+                    Explore
+                </div>
+                <div
+                    className={`${styles.navLink} ${
+                        pathname === '/my-items' ? styles.navLinkActive : null
+                    }`}
                     onClick={() => {
-                        navigate('/connect')
+                        navigate('/my-items')
                     }}
                 >
-                    Connect
-                </button>
-            </div>
-            <div>
-                <img src={menu} className={styles.menu} alt="menu" />
+                    My Items
+                </div>
+                <div
+                    className={`${styles.navLink} ${
+                        pathname === '/following' ? styles.navLinkActive : null
+                    }`}
+                    onClick={() => {
+                        navigate('/following')
+                    }}
+                >
+                    Following
+                </div>
+                <div>
+                    <button
+                        className={styles.button}
+                        onClick={() => {
+                            navigate('/create')
+                        }}
+                    >
+                        Create
+                    </button>
+                </div>
+                <div>
+                    <button
+                        className={styles.buttonOutline}
+                        onClick={() => {
+                            navigate('/connect')
+                        }}
+                    >
+                        Connect
+                    </button>
+                </div>
+                <div>
+                    <img
+                        src={menu}
+                        onClick={() => {
+                            setMobile(true)
+                        }}
+                        className={styles.menu}
+                        alt="menu"
+                    />
+                </div>
             </div>
         </div>
     )
