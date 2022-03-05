c9f5a9e92c62236ac6bbcb317f90a0d260adbdad
Ankit <ankit.rawani.19102001@gmail.com>
2022-03-05 12:15:10 +0530

    Remove: Cards from Home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,10 @@
 import React from 'react'
 // import styles from './Home.module.css'
-import SellerCard from '../SellerCard/SellerCard'
 
 export default function Home() {
     return (
         <div>
-            <SellerCard num="1" imgURL="/img/seller-card-placeholder.png" name="Mia Ayana" eth="0.00" />
+            
         </div>
     );
 }
