ed0f68c03fcb6d132db82f6fbb359cdb1ea755a8
AnandK27 <anandk27@outlook.com>
2022-03-05 12:44:01 +0300

    Footer final

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,84 +1,85 @@
 import React from 'react'
 import styles from './Footer.module.css'
-import logo from '../../images/Dark2.png'
-import fb from "../../images/icons/Social/facebook.png"
-import insta from "../../images/icons/Social/instagram.png"
-import twitter from "../../images/icons/Social/twitter.png"
+import logo from '../../images/Light2.png'
+import insta from "../../images/icons/Social/instagram.svg"
+import twitter from "../../images/icons/Social/twitter.svg"
+import telegram from "../../images/icons/Social/telegram.svg"
+import discord from "../../images/icons/Social/discord.svg"
 
 export default function Footer() {
     return (
+    <section>
     <section className = {styles.footerContainer}>
     <section className={styles.footerWrapper}>
         <section className={styles.footerAboutCol}>
         <section className={styles.footerTitle}><div>
                 <img src={logo} alt="Main Logo" className={styles.footerImage}/>
             </div></section>
+            <section className={styles.footerRowAbout}>
+            <p className={styles.footerEmailHead}>Get the Latest Updates</p>
+            </section>
           <section className={styles.footerRowAbout}>
-              Blah blah blah
+          <div className={styles.searchContainer}>
+                <input
+                    className={styles.searchBar}
+                    type="text"
+                    placeholder="Your Email"
+                />
+            </div>
+
+            <div>
+                <button
+                    className={styles.button}
+                >
+                    Email Me!
+                </button>
+            </div>
+
           </section>        
         </section>
     <section className={styles.footerWrapperInfo}>
         <section className={styles.footerColumn}>
-        <section className={styles.footerTitle}>MarketPlace</section>
+        <section className={styles.footerTitle}>Graphique NFT</section>
           <section className={styles.footerRow}>
-            All NFTs
+            Explore
           </section>
           <section className={styles.footerRow}>
-            Art
+            How it Works
           </section>
           <section className={styles.footerRow}>
-            Photography
-          </section>
-          <section className={styles.footerRow}>
-            Music
+            Contact Us
           </section>
         </section>
         <section className={styles.footerColumn}>
-        <section className={styles.footerTitle}>Company</section>
+        <section className={styles.footerTitle}>Support</section>
           <section className={styles.footerRow}>
-            About
+            Help Center
           </section>
           <section className={styles.footerRow}>
-            Career
+            Terms of Service
           </section>
           <section className={styles.footerRow}>
-            Ventures
-          </section>
-        </section>
-
-        <section className={styles.footerColumn}>
-        <section className={styles.footerTitle}>Resources</section>
-          <section className={styles.footerRow}>
-            Contact
+            Legal
           </section>
           <section className={styles.footerRow}>
-            Partners
-          </section>
-          <section className={styles.footerRow}>
-            Platform Status
+            Privacy Policy
           </section>
         </section>
+ 
         
-        <section className={styles.footerColumn}>
-        <section className={styles.footerTitle}>Socials</section>
-          <section className={styles.footerRowSocial}>
-            <p className={styles.socialPara}><img src={fb} alt="Facebook Logo" className={styles.footerSocialMedia}/>
-             Facebook</p>
-          </section>
-          <section className={styles.footerRowSocial}>
-          <p><img src={insta} alt="Facebook Logo" className={styles.footerSocialMedia}/>
-             Instagram</p>
-          </section>
-          <section className={styles.footerRowSocial}>
-          <p><img src={twitter} alt="Facebook Logo" className={styles.footerSocialMedia}/>
-             Twitter</p>
-          </section>
-        </section>
+    
         </section>
         
     </section>
+    </section>
     <section className={styles.copyright}>
-        &copy; Graphique 2021-21. All right reserved.
+        &copy; Graphique 2021-22. All right reserved.
+        <p style={{float:'right'}}>
+        <img src={insta} alt="Main Logo" className={styles.footerSocialMedia}/>
+        <img src={twitter} alt="Main Logo" className={styles.footerSocialMedia}/>
+        <img src={telegram} alt="Main Logo" className={styles.footerSocialMedia}/>
+        <img src={discord} alt="Main Logo" className={styles.footerSocialMedia}/>
+        </p>
     </section>
     </section>
     )
