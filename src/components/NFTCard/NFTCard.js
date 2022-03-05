0aa441029ddc0f6c21e0ef081ea38f247404fd75
Sneha <sneharmkrshnn@gmail.com>
2022-03-05 13:48:38 +0530

    [add] Homepage

--- src/components/NFTCard/NFTCard.js
+++ src/components/NFTCard/NFTCard.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Icon from '../Icon/Icon';
-import styles from './NFTCard.module.css';
+import React from 'react'
+import Icon from '../Icon/Icon'
+import styles from './NFTCard.module.css'
 
 // USAGE: <NFTCard imgURL="/img/card-placeholder.png" name="NFT Name" amount="0.00" liked likes="92" />
 
@@ -8,17 +8,33 @@ function NFTCard(props) {
     return (
         <div className={styles.card}>
             <div className={styles.imageContainer}>
-                <img src={props.imgURL} alt={props.name+" NFT Image"} />
+                <img
+                    className={styles.image}
+                    src={`/nfts/${props.img}`}
+                    alt={props.name + ' NFT Image'}
+                />
             </div>
             <div className={styles.detailsContainer}>
                 <div className={styles.name}>{props.name}</div>
                 <div className={styles.details}>
-                    <div><span className={styles.num}>{props.amount}</span> <span className={styles.smallText}>ETH</span></div>
-                    <div className={styles.likeButton}><span className={styles.icon}>{props.liked ? <Icon name="heart-filled" size="12" /> : <Icon name="heart" size="12" />}</span><span className={styles.smallText}>{props.likes}</span></div>
+                    <div>
+                        <span className={styles.num}>{props.amount}</span>{' '}
+                        <span className={styles.smallText}>ETH</span>
+                    </div>
+                    <div className={styles.likeButton}>
+                        <span className={styles.icon}>
+                            {props.liked ? (
+                                <Icon name="heart-filled" size="12" />
+                            ) : (
+                                <Icon name="heart" size="12" />
+                            )}
+                        </span>
+                        <span className={styles.smallText}>{props.likes}</span>
+                    </div>
                 </div>
             </div>
         </div>
-    );
+    )
 }
 
-export default NFTCard;
+export default NFTCard
--- src/components/SellerCard/SellerCard.js
+++ src/components/SellerCard/SellerCard.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import styles from './SellerCard.module.css';
+import React from 'react'
+import styles from './SellerCard.module.css'
 
 // USAGE: <SellerCard num="1" imgURL="/img/seller-card-placeholder.png" name="Mia Ayana" eth="0.00" />
 
@@ -8,13 +8,23 @@ function SellerCard(props) {
         <div className={styles.card}>
             <div className={styles.num}>{props.num}</div>
             <div className={styles.dpContainer}>
-                <img className={styles.dp} src={props.imgURL} alt={props.name+"'s image"} />
-                <img className={styles.tick} src="/icons/seller-tick.svg" alt="Tick icon" />
+                <img
+                    className={styles.dp}
+                    src={`/sellers/${props.img}`}
+                    alt={props.name + "'s image"}
+                />
+                <img
+                    className={styles.tick}
+                    src="/icons/seller-tick.svg"
+                    alt="Tick icon"
+                />
             </div>
             <div className={styles.name}>{props.name}</div>
-            <div className={styles.eth}><span className={styles.ethNum}>{props.eth}</span> ETH</div>
+            <div className={styles.eth}>
+                <span className={styles.ethNum}>{props.eth}</span> ETH
+            </div>
         </div>
     )
 }
 
-export default SellerCard;
+export default SellerCard
